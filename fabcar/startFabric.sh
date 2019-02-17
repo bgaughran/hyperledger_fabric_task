9576a4dad9c989e19827db2aec30ab23eff858ef
bgaughran <gaughran@gmail.com>
2019-02-17 20:09:13 +0000

    Added minor documentation

--- fabcar/startFabric.sh
+++ fabcar/startFabric.sh
@@ -17,6 +17,7 @@ CC_SRC_LANGUAGE=`echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:]`
 CC_RUNTIME_LANGUAGE=node # chaincode runtime language is node.js
 CC_SRC_PATH=/opt/gopath/src/github.com/fabcar/javascript
 
+# clean any old docker artefacts lying around
 docker rm -f $(docker ps -aq)
 docker network prune
 docker rmi dev-peer0.org1.example.com-fabcar-1.0-5c906e402ed29f20260ae42283216aa75549c571e2e380f3615826365d8269ba
