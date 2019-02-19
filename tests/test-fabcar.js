774a000d1f564099ae297437619e2ef77d39cfdd
bgaughran <gaughran@gmail.com>
2019-02-19 00:58:21 +0000

    Adding more tests

--- tests/test-fabcar.js
+++ tests/test-fabcar.js
@@ -9,43 +9,122 @@ var assert = chai.assert;
 // get handle to the chaincode
 const chaincode = new ManageDonations();
 
-describe('Test ManageDonations', () => {
+describe('Test ManageDonations', function(){
+    var stub;
 
-    it("Should be able to add & get a donation", async () => {
-        const stub = new ChaincodeMockStub("MyMockStub", chaincode);
+    //reset the stub before each test
+    beforeEach(function(){
+        stub = new ChaincodeMockStub("MyMockStub", chaincode);
 
         //set up the mock
         stub.mockTransactionStart();
+    })
 
-        //call method under test
-        await chaincode.addDonation(stub, 'DisasterServicesCorporation', 'Gas', 1);
+    describe('Test ManageDonations.addDonation', () => {
 
-        //get the donation was added
-        const actualDonation = await chaincode.getDonation(stub, 'DisasterServicesCorporation', 'Gas');
+        it("Should be able to add & get a donation", async () => {
+            //call method under test
+            await chaincode.addDonation(stub, 'DisasterServicesCorporation', 'Gas', 1);
 
-        //assert the donation was correct (except timestamp whose value will change and cannot be asserted)
-        expect(Transform.bufferToObject(actualDonation)).to.deep.include({
-            'docType': 'donation',
-            'projectName': 'DisasterServicesCorporation',
-            'itemType': 'Gas',
-            'amount': 1,
+            //get the donation that was added
+            const actualDonation = await chaincode.getDonation(stub, 'DisasterServicesCorporation', 'Gas');
+
+            //assert the donation was correct (except timestamp whose value will change and cannot be asserted)
+            expect(Transform.bufferToObject(actualDonation)).to.deep.include({
+                'docType': 'donation',
+                'projectName': 'DisasterServicesCorporation',
+                'itemType': 'Gas',
+                'amount': 1,
+            });
+
+            //assert the time stamp of the donation was not null
+            expect(Transform.bufferToObject(actualDonation).lastDonationTimeInMilliseconds).to.be.above(0);
+
         });
 
-        //assert the time stamp of the donation was not null
-        expect(Transform.bufferToObject(actualDonation).lastDonationTimeInMilliseconds).to.be.above(0);
+        it("Should ensure that the donation is incremented", async () => {
+
+            //call method under test
+            await chaincode.addDonation(stub, 'DisasterServicesCorporation', 'Gas', 1);
+            await chaincode.addDonation(stub, 'DisasterServicesCorporation', 'Gas', 1);
+            await chaincode.addDonation(stub, 'DisasterServicesCorporation', 'Gas', 1);
+
+            //get the donation that was added
+            const actualDonation = await chaincode.getDonation(stub, 'DisasterServicesCorporation', 'Gas');
+
+            expect(Transform.bufferToObject(actualDonation).amount).to.equal(3);
+        });
+
+        it("Should add a key/value donation record where one did not exist before", async () => {
+
+//            expect(async function () { await chaincode.getDonation(stub, 'DisasterServicesCorporation', 'Gas'); }).to.throw();  // Function expression
+
+            //verify the key does not exist
+//            const actualDonation = await chaincode.getDonation(stub, 'DisasterServicesCorporation', 'Gas');
+//            expect(Transform.bufferToObject(actualDonation)).to.be.null;
+
+            //now verify our chaincode creates one
+//            await chaincode.addDonation(stub, 'DisasterServicesCorporation', 'Gas', 1);
+//            actualDonation = await chaincode.getDonation(stub, 'DisasterServicesCorporation', 'Gas');
+//            expect(Transform.bufferToObject(actualDonation)).to.be.null;
+
+
+//            expect(getDonationFunction).to.throw(Error);
+//            expect(async function () { await chaincode.getDonation(stub, 'DisasterServicesCorporation', 'Gas'); }).to.throw();  // Function expression
+
+
+            //now check if a new key/value pair is added where one does not exist
+//            const actualDonation = await chaincode.getDonation(stub, 'DisasterServicesCorporation', 'Gas');
+//            expect(Transform.bufferToObject(actualDonation)).to.equal(0);
+
+//            await chaincode.addDonation(stub, 'DisasterServicesCorporation', 'Gas', 1);
+
+        });
 
     });
 
-    it("Should be able to create a key correctly", async () => {
-        const stub = new ChaincodeMockStub("MyMockStub", chaincode);
+    //todo: check expected errors thrown in getDonation (and test getDonation explicitly)
 
-        actualKey = chaincode.createKey('DisasterServicesCorporation', 'Gas');
-        expect(actualKey).to.equal('DisasterServicesCorporation:Gas');
+    describe('Test ManageDonations - helper methods', () => {
+        //todo: test rest of helper methods
 
+        it("Should be able to check a key is valid", async () => {
+
+            var exists = chaincode.isKeyValid(null);
+            expect(exists).to.false;
+
+            exists = chaincode.isKeyValid('');
+            expect(exists).to.false;
+
+            exists = chaincode.isKeyValid('anyKey');
+            expect(exists).to.true;
+
+        });
+
+        it("Should be able to create a key correctly", async () => {
+
+            const actualKey = chaincode.createKey('DisasterServicesCorporation', 'Gas');
+            expect(actualKey).to.equal('DisasterServicesCorporation:Gas');
+
+        });
+
+        it("Should be able to create a value object correctly", async () => {
+
+            const valueObject = chaincode.createDonationValueObject('DisasterServicesCorporation', 'Gas', 99);
+
+            //assert the valueObject is correct (except timestamp whose value will change and cannot be asserted)
+            expect(valueObject).to.deep.include({
+                'docType': 'donation',
+                'projectName': 'DisasterServicesCorporation',
+                'itemType': 'Gas',
+                'amount': 99,
+            });
+
+            //assert the time stamp of the donation was not null
+            expect(valueObject.lastDonationTimeInMilliseconds).to.be.above(0);
+
+        });
     });
 });
 
-//TODO:add test for all methods
-
-//TODO:check Mocha best practices for test layout
 
